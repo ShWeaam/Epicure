c5933544f7f1dee22f856f4d79d0debcd3db90c6
Weaam Shaheen <weaam.2510@gmail.com>
2022-09-19 15:14:15 +0300

    updated dishes route

--- epicure-server/src/routes/index.ts
+++ epicure-server/src/routes/index.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import chefsRoutes from './chefs.routes';
+import dishesRoutes from './dishes.routes';
 import resturantsRoutes from './resturants.routes';
 
 const router = express.Router();
 
 router.use('/chefs/', chefsRoutes);
 router.use('/resturants/', resturantsRoutes);
-// router.use('/dishes/', dishesRoutes);
+router.use('/dishes/', dishesRoutes);
 
 
 export default router;
